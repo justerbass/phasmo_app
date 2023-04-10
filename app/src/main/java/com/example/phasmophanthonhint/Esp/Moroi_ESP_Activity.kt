acef78283e1dceac63ed0294fc82c51f3d17617b
Javier Castillo <jkttzars@gmail.com>
2023-04-10 12:56:13 -0400

    archivos moroi creado

---
+++ app/src/main/java/com/example/phasmophanthonhint/Esp/Moroi_ESP_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.phasmophanthonhint.Esp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.phasmophanthonhint.R
+
+class Moroi_ESP_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moroi_esp)
+    }
+}
