9653e159f93043ba3be000caa8faddacbf811f1c
Javier Castillo <jkttzars@gmail.com>
2023-04-10 11:55:18 -0400

    yokai listo

--- app/src/main/java/com/example/phasmophanthonhint/Esp/Yokai_ESP_Activity.kt
+++ app/src/main/java/com/example/phasmophanthonhint/Esp/Yokai_ESP_Activity.kt
@@ -2,6 +2,7 @@ package com.example.phasmophanthonhint.Esp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.phasmophanthonhint.R
 
 class Yokai_ESP_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
