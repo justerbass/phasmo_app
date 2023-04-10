ad61669bb3e0ccb6c1eec14ed577afb53a854ff7
Javier Castillo <jkttzars@gmail.com>
2023-04-10 09:56:19 -0400

    polter en español listo

--- app/src/main/java/com/example/phasmophanthonhint/SpanishActivity.kt
+++ app/src/main/java/com/example/phasmophanthonhint/SpanishActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import com.example.phasmophanthonhint.Esp.Espiritu_ESP_Activity
+import com.example.phasmophanthonhint.Esp.Poletr_ESP_Activity
 
 class SpanishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +15,13 @@ class SpanishActivity : AppCompatActivity() {
         val BtnEspiritu = findViewById<AppCompatButton>(R.id.Espiritu)
         BtnEspiritu.setOnClickListener {
             val espiritu = Intent(this, Espiritu_ESP_Activity::class.java)
-            startActivity(espiritu)
-        }
+            startActivity(espiritu)}
 
+        val BtnPolter = findViewById<AppCompatButton>(R.id.Poltergeist)
+        BtnPolter.setOnClickListener {
+            val polter = Intent(this, Poletr_ESP_Activity::class.java)
+            startActivity(polter)}
+
+        
     }
 }
