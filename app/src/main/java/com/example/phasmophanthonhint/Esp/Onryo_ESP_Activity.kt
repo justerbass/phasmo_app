9c1498649ff5650d16e2255facd90f666d0e2d59
Javier Castillo <jkttzars@gmail.com>
2023-04-10 17:18:59 -0400

    archivos onryo creados

---
+++ app/src/main/java/com/example/phasmophanthonhint/Esp/Onryo_ESP_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.phasmophanthonhint.Esp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.phasmophanthonhint.R
+
+class Onryo_ESP_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onryo_esp)
+    }
+}
