4676250d98d63abeacaaf0493cec7c13b9558717
Javier Castillo <jkttzars@gmail.com>
2023-04-10 20:35:06 -0400

    archivos oni creados

---
+++ app/src/main/java/com/example/phasmophanthonhint/Esp/Oni_ESP_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.phasmophanthonhint.Esp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.phasmophanthonhint.R
+
+class Oni_ESP_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oni_esp)
+    }
+}
