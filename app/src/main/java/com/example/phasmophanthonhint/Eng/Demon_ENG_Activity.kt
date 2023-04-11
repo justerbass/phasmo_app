7925453c0f3313f3a535ad5aa02b117098760dfd
Javier Castillo <jkttzars@gmail.com>
2023-04-11 12:43:55 -0400

    demon files created

---
+++ app/src/main/java/com/example/phasmophanthonhint/Eng/Demon_ENG_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.phasmophanthonhint.Eng
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.phasmophanthonhint.R
+
+class Demon_ENG_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demon_eng)
+    }
+}
